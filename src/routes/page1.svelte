22632a957646aa0fa9f271dba2b152622c017063
Aled <clement.muzard@etu.umontpellier.fr>
2025-09-26 14:06:14 +0200

    feat: hover + click handle women

--- src/routes/page1.svelte
+++ src/routes/page1.svelte
@@ -1,12 +1,51 @@
+<script>
+  import Back from '../back/1.png';
+  import Femme from '../perso/femme.png';
+
+  function handleClick() {
+    alert('Tu as cliqué sur la femme !');
+  }
+</script>
+
 <style>
-  .background {
+
+
+  main {
     width: 100%;
-    height: 100vh;
-    background-image: url('../back/1.png'); 
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
+    height: 100%;
+    position: relative;
+  }
+
+  .background img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+.femme {
+  position: absolute;
+  top: 400px;
+  left: 850px;
+  cursor: pointer;
+  transition: transform 0.3s ease;
+  border: 10px solid white;
+  background: none;
+  border: 10px solid white;
+  padding: 0;
+}
+
+  .femme:hover {
+    transform: scale(1.1);
   }
 </style>
 
-<div class="background"></div>
+<main>
+  <div class="background">
+    <img src={Back} alt="background" />
+  </div>
+
+<button class="femme" on:click={handleClick} type="button">
+  <img src={Femme} alt="femme" />
+</button>
+
+</main>
