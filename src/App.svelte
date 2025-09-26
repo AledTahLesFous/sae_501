c5340aabb72bce043eda033924abd0c262742a2a
Aled <clement.muzard@etu.umontpellier.fr>
2025-09-26 13:31:21 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Page1 from './routes/page1.svelte';
+  import Router from 'svelte-spa-router';
+
+  const routes = {
+    '/': Page1,
+  };
+</script>
+
+<Router {routes} />
---
+++ src/routes/page1.svelte
@@ -0,0 +1,12 @@
+<style>
+  .background {
+    width: 100%;
+    height: 100vh;
+    background-image: url('../back/1.png'); 
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+</style>
+
+<div class="background"></div>
