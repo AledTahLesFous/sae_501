1aca7a6692c768a67ac13e104983c176be143abf
Atom <atomclem@gmail.com>
2025-09-27 19:52:51 +0200

    fix: dimension container for bg image relative to screens width/height

--- src/routes/page1.svelte
+++ src/routes/page1.svelte
@@ -1,48 +1,22 @@
-<script>
-  import Femme from '../perso/femme.png';
-
-  function handleClick() {
-    alert('Tu as cliqué sur la femme !');
-  }
-</script>
-
 <style>
-  main {
-    position: relative;
+  .container {
     width: 100vw;
     height: 100vh;
+    background-color: purple;
+  }
 
-    background-image: url('../back/1.png');
-    background-size: contain;          /* ✅ change "cover" → "contain" */    background-position: center;
+  .fullscreen-background {
+    width: 100%;
+    height: 100%;
+    background-image: url("../back/1.png");
+    background-size: 100% 100%;
+    background-position: center;
     background-repeat: no-repeat;
-    overflow: hidden;
-  }
-
-  .femme {
-    position: absolute;
-    top: 50%; 
-    left: 50%;
-    transform: translate(-50%, -50%);
-    border: none;
-    background: none;
-    padding: 0;
-    cursor: pointer;
-  }
-
-  .femme img {
-    width: 150px;
-    height: auto;
-    border: 1px solid purple;
-    transition: transform 0.2s ease-in-out;
-  }
-
-  .femme:hover img {
-    transform: scale(1.1);
   }
 </style>
 
 <main>
-  <button class="femme" on:click={handleClick} type="button">
-    <img src={Femme} alt="femme" />
-  </button>
+  <div class="container">
+    <div class="fullscreen-background"></div>
+  </div>
 </main>
