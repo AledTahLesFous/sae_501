0068bf1c1f68aad417bde589b83d089770f5738e
Atom <atomclem@gmail.com>
2025-09-27 20:26:44 +0200

    feat: add women & position

--- src/routes/page1.svelte
+++ src/routes/page1.svelte
@@ -1,10 +1,10 @@
 <style>
   .container {
     position: absolute;
-
     width: 100vw;
     height: 100vh;
     background-color: purple;
+    overflow: hidden; /* évite les débordements éventuels */
   }
 
   .fullscreen-background {
@@ -20,23 +20,22 @@
   
   .women {
     position: absolute;
-    width: 100%;
-    height: 100%;
-    background-position: center;
-    background-image: url("../perso/femme.png");
-    background-repeat: no-repeat;
-    z-index: 1;
     top: 17%;
     left: 6%;
-
-
+    width: 30vw; /* taille adaptative */
+    height: auto; /* respecte les proportions */
+    background-image: url("../perso/femme.png");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: contain;
+    z-index: 1;
+    pointer-events: none; /* optionnel si tu ne veux pas interagir avec */
   }
 </style>
 
 <main>
   <div class="container">
     <div class="fullscreen-background"></div>
-
     <div class="women"></div>
   </div>
 </main>
