a76d72b4f50a597b27358f6724150037518c8ac7
Atom <atomclem@gmail.com>
2025-10-04 00:42:02 +0200

    feat: display screen ration @ women, test

--- src/routes/page1.svelte
+++ src/routes/page1.svelte
@@ -19,17 +19,24 @@
   }
   
   .women {
-    position: absolute;
-    width: 100%;
+    width: 100%; /* taille "originale" */
     height: 100%;
-    background-position: center;
     background-image: url("../perso/femme.png");
     background-repeat: no-repeat;
+    background-size: contain;
+    background-position: center;
+    transform-origin: top left;
+    transform: scale(0.6);
     z-index: 1;
-    top: 17%;
-    left: 6%;
+    position: absolute;
+    top: 38%;
+    left: 25%;
+  }
 
-
+  @media (max-width: 1920px) {
+    .women {
+      transform: scale(calc(0.2 + (100vw - 480px) / (1920 - 480) * (0.8 - 0.2)));
+    }
   }
 </style>
 
