f6bfbb6bbe7ec518ab603adebda68dced54368a9
Atom <clement.muzard@etu.umontpellier.fr>
2025-10-06 11:06:26 +0200

    fix: on object fit contain

--- src/routes/page1.svelte
+++ src/routes/page1.svelte
@@ -1,44 +1,45 @@
+<script>
+  import backgroundImage from '../back/1.png';
+  import womanImage from '../perso/femme.png';
+</script>
+
 <style>
   .container {
-
     width: 100vw;
     height: 100vh;
     background-color: purple;
     z-index: 5;
+    position: relative;
+    overflow: hidden;
+    object-fit: cover;
+
   }
 
   .fullscreen-background {
     position: absolute;
     width: 100%;
     height: 100%;
-    background-image: url("../back/1.png");
-    background-size: 100% 100%;
-    background-position: center;
-    background-repeat: no-repeat;
     z-index: 0;
   }
-  
+
   .women {
-    width: 100%; /* taille "originale" */
+    width: 100%;
     height: 100%;
-    background-image: url("../perso/femme.png");
+    background-image: url("");
     background-repeat: no-repeat;
     background-size: contain;
     background-position: center;
-    transform-origin: top left;
+    transform-origin: center center;
     transform: scale(0.6);
     z-index: 1;
     position: absolute;
     top: 17%;
     left: 5%;
     transition: transform 0.3s ease;
-    transform-origin: center center; /* nouveau */
-
-
   }
 
   .women:hover {
-  transform: scale(0.66); /* 0.6 + un petit peu (~10%) */
+    transform: scale(0.66);
   }
 
   @media (max-width: 1920px) {
@@ -47,20 +48,19 @@
     }
   }
 
-    @media (max-height: 1080px) {
+  @media (max-height: 1080px) {
     .women {
       transform: scale(calc(0.2 + (100vh - 480px) / (1080 - 480) * (0.8 - 0.2)));
     }
   }
-
 </style>
 
 <main>
   <div class="container">
-    <div class="fullscreen-background"></div>
-    <div class="container">
-          <div class="women"></div>
-    </div>
+    <!-- Background avec object-fit -->
+    <img src={backgroundImage} alt="Background" class="fullscreen-background" />
 
+    <!-- Personnage féminin -->
+    <div class="women" style="background-image: url({womanImage})"></div>
   </div>
 </main>
