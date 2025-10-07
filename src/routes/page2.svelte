b02492a8a0ff359924e6b743181e5f604db80932
Atom <clement.muzard@etu.umontpellier.fr>
2025-10-07 13:04:28 +0200

    feat: add spin for interaction

--- src/routes/page2.svelte
+++ src/routes/page2.svelte
@@ -1,8 +1,8 @@
 <script>
   import { onMount } from 'svelte';
+  import { writable } from 'svelte/store';
 
   import backgroundImage from '../backgrounds/page2/bg_2.png';
-
 
   let currentBg = backgroundImage;
 
@@ -17,11 +17,52 @@
 
     return () => clearInterval(interval);
   });
+
+  // Spinner
+  let spinnerVisible = true;
+  let rotation = 0; // angle du spinner
+  let lastAngle = null;
+  let rotationsCount = 0;
+
+  function handleMouseMove(event) {
+    if (!spinnerVisible) return;
+
+    const circle = document.getElementById('spinner');
+    const rect = circle.getBoundingClientRect();
+    const centerX = rect.left + rect.width / 2;
+    const centerY = rect.top + rect.height / 2;
+
+    const dx = event.clientX - centerX;
+    const dy = event.clientY - centerY;
+
+    const angle = Math.atan2(dy, dx) * (180 / Math.PI); // angle en degrés
+
+    if (lastAngle !== null) {
+      let delta = angle - lastAngle;
+
+      // Correction pour rotation continue
+      if (delta > 180) delta -= 360;
+      if (delta < -180) delta += 360;
+
+      rotation += delta;
+
+      // Compte les tours complets
+      if (Math.abs(rotation) >= 360) {
+        rotationsCount += Math.floor(Math.abs(rotation) / 360);
+        rotation = rotation % 360;
+
+        if (rotationsCount >= 6) {
+          spinnerVisible = false;
+        }
+      }
+    }
+
+    lastAngle = angle;
+  }
 </script>
 
+
 <style>
-
-
 
 .textbox {
   width: 750px;
@@ -39,21 +80,39 @@
   justify-content: space-around;
 }
 
-
+#spinner {
+  position: absolute;
+  width: 350px;
+  height: 350px;
+  border: 6px solid rgb(0, 0, 0, 0.8);
+  background-color: rgba(47, 47, 47, 0.23);
+  border-radius: 50%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%) rotate(0deg);
+  cursor: grab;
+  z-index: 2;
+  user-select: none;
+}
 
 </style>
 
-<main>
+
+<main on:mousemove={handleMouseMove}>
   <div class="container">
     <!-- Background -->
     <img src={currentBg} alt="Background" class="fullscreen-background" />
 
+    <!-- Texte -->
+    <div class="textbox">Quand on l’obligeait de filer</div>
 
-  
-    <div class="textbox">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam non molestie nibh. Aenean quis semper lacus, quis finibus enim.</div>
-
-    <div class="bt">
-      <div class="hidetext"></div>
-    </div>
+    <!-- Spinner -->
+    {#if spinnerVisible}
+      <div 
+        id="spinner"
+        style="transform: translate(-50%, -50%) rotate({rotation}deg);">
+      </div>
+    {/if}
   </div>
 </main>
+
