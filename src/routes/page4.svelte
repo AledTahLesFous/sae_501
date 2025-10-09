df044e34ad0679f4132abe722b2e4ea46cdf514a
Atom <clement.muzard@etu.umontpellier.fr>
2025-10-09 09:31:47 +0200

    feat: add page 4

---
+++ src/routes/page4.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { onMount } from 'svelte';
+
+  import { textboxVisible } from '../stores.js';
+  import backgroundImage from '../backgrounds/page4/bg_4.png';
+
+
+</script>
+
+<style>
+
+
+</style>
+
+
+<main>
+  <div class="container">
+    <!-- Background -->
+    <img src={backgroundImage} alt="Background" class="fullscreen-background" />
+
+       {#if $textboxVisible}
+
+    <div class="textbox2">Plutôt que de démêler les petits pelotons de filasse qui se rencontraient dans le lin</div>
+    {/if}
+
+  </div>
+</main>
+
