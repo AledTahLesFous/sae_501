2d20299b5d4846fdee5f625080dc4454636d27b3
Atom <clement.muzard@etu.umontpellier.fr>
2025-10-15 11:33:01 +0200

    feat: interaction no more bg frame animation but on click

--- src/routes/page3.svelte
+++ src/routes/page3.svelte
@@ -1,45 +1,59 @@
 <script>
-  import { onMount } from 'svelte';
-
   import { textboxVisible } from '../stores.js';
   import backgroundImage from '../backgrounds/page3/bg_3.avif';
   import backgroundImage2 from '../backgrounds/page3/bg_3_f2.avif';
 
   let currentBg = backgroundImage;
+  let currentText = "Elle s’en acquittait avec tant d’ennui";
 
-  onMount(() => {
-    const images = [backgroundImage, backgroundImage2];
-    let index = 0;
+  // Quand on clique sur la hitbox
+  function handleClick() {
+    // Change l'image et le texte
+    currentBg = backgroundImage2;
+    currentText = "Hmpf";
 
-    const interval = setInterval(() => {
-      index = (index + 1) % images.length; 
-      currentBg = images[index];
-    }, 700);
-
-    return () => clearInterval(interval);
-  });
-
+    // Après 2 secondes, redirige vers /page4
+    setTimeout(() => {
+      window.location.hash = "#/page4";
+    }, 2000);
+  }
 </script>
-
 
 <style>
 
 
+  .hitbox {
+    width: 35%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 20%;
+    border: 1px solid purple;
+    cursor: pointer;
+    z-index: 10;
+  }
 
 </style>
 
-
 <main>
   <div class="container">
-    <!-- Background -->
-        {#key currentBg}
+    <!-- Image de fond -->
+    {#key currentBg}
       <img src={currentBg} alt="Background" class="fullscreen-background" />
     {/key}
-       {#if $textboxVisible}
 
-    <div class="textbox2">Elle s’en acquittait avec tant d’ennui</div>
+    <!-- Zone cliquable -->
+    <!-- svelte-ignore a11y_click_events_have_key_events -->
+    <!-- svelte-ignore a11y_no_static_element_interactions -->
+    <div
+      class="hitbox"
+      on:click={handleClick}
+      aria-label="Aller à la page 4"
+    ></div>
+
+    <!-- Texte -->
+    {#if $textboxVisible}
+      <div class="textbox2">{currentText}</div>
     {/if}
-
   </div>
 </main>
-
