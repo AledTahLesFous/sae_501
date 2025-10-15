f18bc0d9a83892ef11f9db97c8b7f4d436eb7033
Atom <clement.muzard@etu.umontpellier.fr>
2025-10-15 16:30:17 +0200

    feat: page 11

---
+++ src/routes/page11.svelte
@@ -0,0 +1,76 @@
+<script>
+// @ts-nocheck
+
+
+  import { onMount } from 'svelte';
+
+  import { textboxVisible } from '../stores.js';
+  import backgroundImage from '../backgrounds/page11/bg_11.png';
+  import servanteImage from '../assets/page11/servante.png';
+
+  let currentBg = backgroundImage;
+
+  onMount(() => {
+    const images = [backgroundImage];
+    let index = 0;
+
+    const interval = setInterval(() => {
+      index = (index + 1) % images.length; 
+      currentBg = images[index];
+    }, 700);
+
+    return () => clearInterval(interval);
+  });
+
+</script>
+
+
+<style>
+
+  .container-flex {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    z-index: 5;
+    background-size: cover;
+    flex-direction: column;
+    justify-content: end;
+    align-items: center; 
+  }
+
+  .women {
+    position: absolute;
+    width: 25%;
+    height: 75%;
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    transform-origin: center center;
+    z-index: 1;
+    transition: transform 0.3s ease;
+  }
+
+  .women:hover {
+    transform: scale(1.02);
+  }
+
+</style>
+
+
+<main>
+  <div class="container">
+    <!-- Background -->
+    <img src={backgroundImage} alt="Background" class="fullscreen-background" />
+    <div class="container-flex">
+  <!-- svelte-ignore a11y_click_events_have_key_events -->
+  <!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+  <img src={servanteImage} alt="servanteImage" class="women" on:click={window.location.hash = '#/page11'}/>
+    </div>
+
+    {#if $textboxVisible}
+    <div class="textbox">Et elle s’en fit faire une jolie robe</div>
+    {/if}
+
+  </div>
+</main>
+
