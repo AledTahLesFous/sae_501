08558045ffa15a0f3c7ae52dc5b114476f65cad3
Atom <clement.muzard@etu.umontpellier.fr>
2025-10-20 15:35:22 +0200

    Relance du déploiement GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
   plugins: [svelte()],
-  base: process.env.NODE_ENV === 'production' ? '/nom-du-repo/' : '/'
+  base: process.env.NODE_ENV === 'production' ? '/sae_505/' : '/'
 })
